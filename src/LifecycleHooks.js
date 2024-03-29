cab4aff38c5d5e9a8ec611aa3addfab1221b6ae1
RileyLueilwitz <alvinmcdonald2bcd65054392@outlook.com>
2024-03-29 08:37:00 +0800

    autem

---
+++ src/LifecycleHooks.js
@@ -0,0 +1,40 @@
+
+export function getHooks(echarts) {
+    return {
+        mounted() {
+            const inst = echarts.init(this.$el, this.initTheme, this.initOpts);
+            this.$data._private.dynamic.inst = inst;
+            
+            if (this.loading) {
+                inst.showLoading(this.loadingType, this.loadingOpts)
+            }
+            if (this.option) {
+                this.setOption(this.option, this.setOptionOpts);
+            }
+            if (this.events) {
+                this.events.forEach(args => inst.on(...args));
+            }
+            if (this.autoResize) {
+                this.addResizeListener();
+            }
+        },
+        
+        activated() {
+            if (this.autoResize) {
+                this.addResizeListener();
+            }
+        },
+        
+        deactivated() {
+            if (this.autoResize) {
+                this.removeResizeListener();
+            }
+        },
+        
+        beforeUnmount() {
+            this.removeResizeListener();
+            this.inst.dispose();
+        },
+        
+    };
+}
